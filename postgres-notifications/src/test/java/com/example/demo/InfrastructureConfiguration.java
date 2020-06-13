f75a7b31f6d4505415fd391f8544d177fc6480ba
Víctor Martín Molina <vmartin@profile.es>
2020-06-13 17:55:49 +0200

    executesFindAll test

---
+++ postgres-notifications/src/test/java/com/example/demo/InfrastructureConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+class InfrastructureConfiguration {}
